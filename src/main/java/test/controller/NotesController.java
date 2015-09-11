5aa9e17f65e18678b505a988074f9994b39dd5a3
Artur Belski <abelski@exadel.com>
2015-09-11 12:27:03 +0300

    edit todo

--- src/main/java/test/controller/NotesController.java
+++ src/main/java/test/controller/NotesController.java
@@ -1,12 +1,14 @@
 package test.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import test.model.Todo;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.annotation.PostConstruct;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Artur Belski
@@ -14,18 +16,27 @@ import java.util.List;
 @Controller
 @RequestMapping("/todo")
 public class NotesController {
+    final Map<String, Todo> todos = new HashMap<String, Todo>();
+
+    @PostConstruct
+    public void init() {
+        todos.put("1", new Todo("1","do some", true));
+        todos.put("2", new Todo("2","do some else", false));
+        todos.put("3", new Todo("3","do nothing", true));
+        todos.put("4", new Todo("4","do homework", false));
+        todos.put("5", new Todo("5","do it", true));
+    };
+
     @ResponseBody
-    @RequestMapping(value = "/")
-    public List<Todo> getAll() {
-        final ArrayList<Todo> todos = new ArrayList<Todo>();
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public Collection<Todo> getAll() {
+        return todos.values();
+    }
 
-        todos.add(new Todo("do some",true));
-        todos.add(new Todo("do some else",false));
-        todos.add(new Todo("do nothing",true));
-        todos.add(new Todo("do homework",false));
-        todos.add(new Todo("do it",true));
-
-        return todos;
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public void update(@PathVariable String id, @RequestBody Todo todo) {
+        todos.put(id,todo);
     }
 
 }
--- src/main/webapp/resources/views/EditTodoItemView.js
+++ src/main/webapp/resources/views/EditTodoItemView.js
@@ -1,17 +1,25 @@
-define(['Backbone', 'Underscore', 'jQuery', 'text!templates/editmodal.html'], function (Backbone, _, $, modalTemplate) {
+define(['Backbone', 'Underscore', 'jQuery', 'text!templates/editmodal.html', 'collections/ToDoCollection'], function (Backbone, _, $, modalTemplate, ToDoCollection) {
     return Backbone.View.extend({
         initialize: function (options) {
             _.bindAll(this, 'render');
             this.render();
             this.model = options.model;
+
+            this.listenTo(this.model,'sync', this.onSuccessCallback);
         },
         events: {
             "submit": "save",
             "click #close": "close",
         },
 
+        onSuccessCallback : function(){
+            this.model.fetch();
+            this.render();
+        },
+
         save: function () {
-            alert('savw');
+            this.model.set("name", $("#modalText").val());
+            this.model.save();
             this.closeForm(this);
         },
         close: function closeform() {
